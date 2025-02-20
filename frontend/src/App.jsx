e817df099881cd1eec50d686ab27cead065d0a7b
ArsalJafri <154714497+ArsalJafri@users.noreply.github.com>
2025-02-20 14:06:13 -0600

    Created a small boilerplate for us to all start working on

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Ticket from './pages/Ticket';
+import Home from './pages/Home';
 
-const App = () => {
-
+function App() {
   return (
-    <>
-    Start from scratch Muesuem DB. 
-    </>
-      )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home page for root route */}
+        <Route path="/tickets" element={<Ticket />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+
+
---
+++ frontend/src/pages/Ticket.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Ticket() {
+    return (
+      <div className="ticket-container">
+        <h1 className="ticket-title">Museum Tickets</h1>
+      </div>
+    );
+  }
+  
+  
