5809b94b945ca4dc462c1c5b45882ce64c2d08bd
gwmeadow <willm6461@gmail.com>
2025-02-20 16:37:39 -0600

    "Added Exhibitions function to Exhibition.jsx and  imported it to App.jsx"

---
+++ frontend/src/pages/Exhibitions.jsx
@@ -0,0 +1,11 @@
+
+function Exhibitions(){
+    return(
+        <div className = "Exhibitions">
+            <h1 className ="Exhibit1">Exhibit 1</h1>
+            <p>Show something off here!</p>
+        </div>
+    );
+};
+
+export default Exhibitions;
