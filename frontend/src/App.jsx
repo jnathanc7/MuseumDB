67a0b628b52d7ccc55e59a3d88a7713104835f20
DucPham5 <138059092+DucPham5@users.noreply.github.com>
2025-02-23 19:33:57 -0600

    Setting up the giftshop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Ticket from './pages/Ticket';
 import Home from './pages/Home';
 import Exhibitions from './pages/Exhibitions';
+import Giftshop from './pages/Giftshop';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} /> {/* Home page for root route */}
         <Route path="/tickets" element={<Ticket />} />
         <Route path="/Exhibitions" element={<Exhibitions/>}/>
+        <Route path = "/Giftshop" element = {<Giftshop/>}/>
       </Routes>
     </Router>
   );
