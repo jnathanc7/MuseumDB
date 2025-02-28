2988cf1eaf479b3360d7d1024f54514de2f1adc8
DucPham5 <138059092+DucPham5@users.noreply.github.com>
2025-02-27 23:01:07 -0600

    saving current giftshop state

--- frontend/src/pages/Giftshop.jsx
+++ frontend/src/pages/Giftshop.jsx
@@ -7,19 +7,20 @@ const Giftshop = () =>{
     has authorization to update the store if access is approved then show button that 
     updates array if not dont show and present the current state of the array */
     /*categories is the current state, setCategories will update the state of the array */
-    const [categories, setCategories] = useState([{id:1, name: "Paintings"},
-                                                   {id:2, name: "Jewlery"}, 
-                                                   {id:3, name:"Books"},
-                                                   {id:4, name:"Toys"},
-                                                   {id:5, name:""},
-                                                   {id:6, name:""}]);
+    /*instead of hard coding data make new entity in data base and fetch the info from the table */
+    const [categories, setCategories] = useState([{id:1, name: "Paintings", img: "/paintings.jpg"},
+                                                   {id:2, name: "Jewlery", img: "/Jewlery.jpg"}, 
+                                                   {id:3, name:"Books", img: "/books.jpg"},
+                                                   {id:4, name:"Toys", img: ""},
+                                                   {id:5, name:"", img: ""},
+                                                   {id:6, name:"", img: ""}]);
     return(
     <div className = "homepage">
         {/* <h1>this is the giftshop!</h1> */}
         <div className = "giftshop-container"> 
             {categories.map((category) => (/*maps through the array map calls the function on every element in the array */
                 /*creates a new category for each index */
-                <div key = {category.id} className = "category">
+                <div key = {category.id} className = "category" style = {{backgroundImage: `url(${category.img})`}}>
                     <p className = "category-text">{category.name}</p>
                 </div>
             ))}
