a12ccf7723c321943467f23715301aaebeb28b8f
DucPham5 <138059092+DucPham5@users.noreply.github.com>
2025-03-24 17:04:30 -0500

    polish one last time

--- frontend/src/pages/Giftshop.jsx
+++ frontend/src/pages/Giftshop.jsx
@@ -45,7 +45,7 @@ const Giftshop = () =>{
             {/*Hero Section */}          
             <div className = "giftshop-hero" >
             <h1 className = "giftshophero-header">Welcome to the Museum Giftshop</h1>
-            <p className = "giftshophero-text">Discover a curated selection of art prints, handcrafted jewelry, unique souvenirs, and more. Take home a piece of history today!</p>
+            <p className = "giftshophero-text">Discover a curated selection of art prints, handcrafted jewelry, unique souvenirs, and more!</p>
             </div>
 
             {/* Categories Section */}
