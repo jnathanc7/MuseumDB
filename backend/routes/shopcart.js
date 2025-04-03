d2b4e777587a82b903ebe1316487b7c0c2b99d02
DucPham5 <138059092+DucPham5@users.noreply.github.com>
2025-04-03 16:11:47 -0500

    saving

--- backend/routes/shopcart.js
+++ backend/routes/shopcart.js
@@ -7,7 +7,7 @@ module.exports = (req, res) => {
     const method = req.method;
 
         // Handle CORS
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5183");
+    res.setHeader("Access-Control-Allow-Origin", "https://museum-db-kappa.vercel.app");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
